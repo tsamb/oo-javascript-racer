c479188eb49b170f81904c27f4e0cd7e304f9693
Sam Blackman <sblackman@gmail.com>
2014-09-24 13:58:38 -0400

    myles changes and comments

--- racer.js
+++ racer.js
@@ -1,66 +1,51 @@
 $(document).ready(function(){
-  new Game
+  Game.start(BlockingDataCollection.getPlayerNamesAndKeys())
+  // Game.start([[]])
 });
 
-function getGameInfo() {
-  var numberOfPlayers = prompt("How many players?");
-  var players = setPlayerDetails(numberOfPlayers);
-  return players
-};
-
-function setPlayerDetails(num) {
-  var players = [];
-  for ( i=0; i < num; i++ ) {
-    var name = prompt("Player " + (i+1) + ", what is your name?");
-    var key = prompt( name + ", please choose your key.").charCodeAt(0) - 32;
-    players[i] = new Player(i+1, name, key);
-  }
-  return players;
-};
-
-function Player(num, name, key) {
-  this.name = name;
-  this.num = num;
-  this.key = key;
-  this.position = 0;
-};
-
-Player.prototype.move = function() {
-  this.$track.children().eq(this.position).removeClass("active")
-  this.position += 1;
-  this.$track.children().eq(this.position).addClass("active")
-};
-
-function listenForKeys(players) {
-  $.each(players, function(index, player) {
-    $(document).keyup(function(event) {
-      if (event.keyCode == player.key) {
-        player.move();
-      }
-    });
-  });
-}
-
-function Game(players) {
-  this.players = getGameInfo();
+function Game(options) {
+  // invariants
+  this.players = getGameInfo(); // disentangle by modularizing as per driver code above /
   this.container = $(".racer-table");
-  this.trackLength = 20;
+  this.trackLength = options.trackLength || 20;
+
+  // gamestate
   this.finished = false;
   this.winner = null;
-  this.drawBoard();
-  this.handleKeyUp()
+
+  // this.drawBoard();
+  // this.handleKeyUp();
+}
+
+Game.start(options) {
+  var g = new Game(options)
+  g.drawBoard()
+  g.handleKeyUp()
 }
 
 Game.prototype.drawBoard = function() {
   var game = this;
-  $.each(this.players, function(index, player) {
-    game.container.append("<tr id='" + player.name + "-track'></tr>");
+  // $.each(this.players, function(index, player) {
+  this.players.forEach(function(player, index) {
+    game.container.append("<tr id='" + player.name + "-track'></tr>"); // disentangle this into createElement namespace
+    var remainingTrackPiecesToDraw = this.trackLength;
+
+    var rowsHTML = ''
+    while (remainingTrackPiecesToDraw--) rowsHTML += "<td></td>"
+    range(0,this.trackLength).reduce
+
     player.$track = $("#" + player.name + "-track");
     for (i = 0; i < game.trackLength; i++) {
       player.$track.append("<td></td>");
     }
-    player.$track.children().first().addClass("active")
+    this.activateFirstCellForPlayer()
+    // player.$track.children().first().addClass("active");
+    player.$track.find('.cell:first-child').addClass("active"); // game logic shouldn't know/care about the exact structure of the DOM
   });
+}
+
+Game.prototype.handleEvents = function() {
+  this.focusableElement.on('keyup', extractLetterPressed(movePlayerFromKey))
 }
 
 Game.prototype.handleKeyUp = function() {
@@ -86,4 +71,64 @@ Game.prototype.finishEvent = function(winningPlayer) {
   $(document).off(); // unbind key listeners
   this.finished = true;
   this.winner = winningPlayer;
+  // TODO: send winning player info to view
 }
+
+function Player(num, name, key) {
+  this.name = name;
+  this.num = num;
+  this.key = key;
+  this.position = 0;
+};
+
+Player.prototype.move = function() {
+  this.$track.children().eq(this.position).removeClass("active")
+  this.position += 1;
+  this.$track.children().eq(this.position).addClass("active")
+};
+
+function getGameInfo() {
+  var numberOfPlayers = prompt("How many players?");
+  var players = createPlayers(numberOfPlayers);
+  return players;
+};
+
+function createPlayers(num) {
+  var players = [];
+  for ( i=0; i < num; i++ ) {
+    var name = BlockingDataCollection.nameForNumericallyIndexedPlayer(i+1);
+    var key = prompt( name + ", please choose your key.").charCodeAt(0) - 32;
+    players[i] = new Player(i+1, name, key);
+  }
+  return players;
+};
+
+var playerNamesAndKeys = BlockingDataCollection.getPlayerNamesAndKeys()
+
+// playerNamesAndKeys
+[[ 'myles', 'm' ], ['sam', 's' ] ]
+
+
+// ---
+
+var BlockingDataCollection = {}
+
+BlockingDataCollection.nameForNumericallyIndexedPlayer = function(num) {
+  return prompt("Player " + num + ", what is your name?");
+}
+
+BlockingDataCollection.getPlayerNamesAndKeys = function() {
+  var players = [];
+  var count = parseInt(this.playerCount());
+  for (i = 1; i <= count; i++) {
+    players.push(this.nameForNumericallyIndexedPlayer(i));
+  }
+  return players;
+}
+
+BlockingDataCollection.playerCount = function() {
+  return prompt("How many players?");
+}
+// ---
+
+
